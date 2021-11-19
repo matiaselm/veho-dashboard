bc2cf2f9fc35d054a19793e139b034806e02267b
matiaselm <matias@elm.fi>
2021-11-19 15:36:43 +0200

    render cars on own page

--- src/components/ModifyForm.js
+++ src/components/ModifyForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const ModifyForm = ({ onSubmit, modify, target }) => {
+const ModifyForm = ({ onSubmit, modify, target, onClose }) => {
   const [form, setForm] = useState(modify);
 
   useEffect(() => {
@@ -20,11 +20,11 @@ const ModifyForm = ({ onSubmit, modify, target }) => {
     setForm(null)
   }
 
-  return (<div>
-    <p>{target}: {modify.name} {modify.model}</p>
+  return (<div style={{ maxWidth: 400, padding: 10 }}>
+    <p>{modify.name} {modify.manufacturer} {modify.model} { onClose && <button onClick={onClose}>Peruuta</button>}</p>
     <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
       {form && Object.keys(form).map((key, i) => {
-        if(key === '__v') {
+        if( key=== '_id' || key === '__v') {
           return null
         } else if (key === 'starts_at' || key === 'ends_at') {
           return <input type="date" name={key} placeholder={key} value={form[key]} onChange={handleChange} key={i} style={{ margin: 5 }} />
--- src/pages/Cars.js
+++ src/pages/Cars.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import InputForm from '../components/InputForm'
+import ModifyForm from '../components/ModifyForm'
+import Box from '../components/Box';
 
 const styles = {
   card: {
@@ -11,22 +14,56 @@ const styles = {
 
 const Cars = (props) => {
   const [cars, setCars] = useState(null)
+  const [modify, setModify] = useState(null);
+  const [addFormVisible, setAddFormVisible] = useState(false);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/cars').then(res => {
-      setCars(res.data)
-    }).catch(err => console.log(err))
+    getCars()
   }, [])
+
+  const getCars = async () => {
+    axios.get(`http://localhost:3000/cars`).then(res => {
+      setCars(res.data)
+    }).catch(err => console.error(err))
+  }
+
+  const onModify = () => {
+    axios.post(`http://localhost:3000/cars/${modify._id}`, modify).then(res => {
+      console.log(res)
+      getCars()
+    }).catch(err => console.error(err))
+  }
+
+  const onSubmit = (body) => {
+    axios.post(`http://localhost:3000/cars`, body).then(res => {
+      console.log(res)
+      getCars()
+    }).catch(err => console.error(err))
+  }
+
+  const onRemove = (car) => {
+    const c = window.confirm(`Remove ${car.manufacturer} ${car.model}?`)
+    if(c) axios.delete(`http://localhost:3000/cars/${car._id}`).then(res => {
+      console.log(res)
+      getCars()
+    }).catch(err => console.error(err))
+  }
 
   return <div>
     <h1>Cars</h1>
+    {modify && <ModifyForm modify={modify} target='cars' onSubmit={onModify} onClose={() => setModify(null)}/>}
+    {addFormVisible && <InputForm target='cars' onSubmit={onSubmit} />}
     <div>
-      {(Array.isArray(cars) && cars.length > 0) && cars.map(car => {
-        return <div key={car.id} style={styles.card}>
-          <p>{car.name} {car.manufacturer} {car.model}</p>
-          <p>{car.year} {car.km} {car.fueltype}</p>
-        </div>
-      })}
+      Autot <button onClick={getCars}>Refresh</button><button onClick={() => setAddFormVisible(!addFormVisible)}>Add</button>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {(Array.isArray(cars) && cars.length > 0) && cars.map(car => {
+          return <Box key={car._id} image_url={car.image_url} onPressModify={() => setModify(car)} onPressRemove={() => onRemove(car)} >
+            <p>{car.name} {car.manufacturer} {car.model}</p>
+            <p>{car.year} {car.km} {car.fueltype}</p>
+            <p>ID: {car._id}</p>
+          </Box>
+        })}
+      </div>
     </div>
   </div>
 }
