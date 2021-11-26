b7db6a795e0f29ad21004c68087cd7559029415c
Matias Jalava <matias@elm.fi>
2021-11-26 02:48:19 +0200

    api url to variable

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_API_TOKEN)
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + process.env.REACT_APP_API_TOKEN
